44117feeca6d4ea5e6e2e245d16b58a5e42a61ac
Dasthu <118829965+kuspilla@users.noreply.github.com>
2023-11-30 08:24:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 const makeChange = (c) => {
   // your name here
+       let cents = c;
+	const quarterValue = 25;
+  const dimeValue = 10;
+  const nickelValue = 5;
+  const pennyValue = 1;
+
+  // Initialize variables to count the number of each coin
+  let quarters = 0;
+  let dimes = 0;
+  let nickels = 0;
+  let pennies = 0;
+
+  // Calculate the number of each coin needed
+  while (cents > 0) {
+    if (cents >= quarterValue) {
+      quarters++;
+      cents -= quarterValue;
+    } else if (cents >= dimeValue) {
+      dimes++;
+      cents -= dimeValue;
+    } else if (cents >= nickelValue) {
+      nickels++;
+      cents -= nickelValue;
+    } else {
+      pennies = cents;
+      cents = 0;
+    }
+  }
+
+  // Create and return the result object
+  const result = {
+    q: quarters,
+    d: dimes,
+    n: nickels,
+    p: pennies
+  };
+
+  return result;
 };
 
 // Do not the change the code below
